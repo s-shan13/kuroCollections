b4c696b9e1ce3f3f6a3b56d4a7c588473940ac9e
shan <shantanusuwarnkar@gmail.comD>
2022-07-27 17:42:53 +0100

    Added config and route template

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
+//Change start to node in json
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//Route imports
+const productRouter = require("./routes/productRoute")
+//config
+dotenv.config({path:"backend/config/config.env"})
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/v1", productRouter)
 
 app.get('/', (req, res) => {
     res.send('backend')
 })
 
-app.listen(5000)
+app.listen(PORT)
