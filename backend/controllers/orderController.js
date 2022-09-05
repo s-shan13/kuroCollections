55e7d9902c1fc1ac474a113e79cfc65a31c48cff
shan <shantanusuwarnkar@gmail.comD>
2022-09-05 18:43:29 +0100

    Added functions to update and delete orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -49,3 +49,70 @@ exports.myOrders = catchAsyncErrors(async(req,res,next)=>{
         orders
     })
 })
+
+/**
+ * Get all orders
+ */
+exports.getAllOrders = catchAsyncErrors(async(req,res,next)=>{
+    
+    const orders = await Order.find()
+
+    let totalAmount = 0;
+    orders.forEach(order=>{
+        totalAmount += order.totalPrice 
+    })
+
+    res.status(200).json({
+        success:true,
+        totalAmount,
+        orders
+    })
+})
+
+/**
+ * update order status
+ */
+exports.updateOrderStatus = catchAsyncErrors(async(req,res,next)=>{
+    
+    const order = await Order.findById(req.params.id)
+
+    if(order.orderStatus==="Delivered"){
+        return next(new ErrorHandler("Already delivered", 404))
+    }
+
+    order.orderItems.forEach(async (order)=>{
+        await updateStock(order.product, order.quantity)
+    })
+
+    order.orderStatus = req.body.status;
+    if(req.body.status === "Delivered"){
+        order.deliveredAt=Date.now()
+    }
+    await order.save({validateBeforeSave:false})
+    res.status(200).json({
+        success:true,
+        order
+    })
+})
+
+async function updateStock(id, quantity){
+    const product = await Product.findById(id);
+
+    product.stock -= quantity
+    await product.save({validateBeforeSave:false})
+}
+
+/**
+ * Delete
+ */
+exports.deleteOrder = catchAsyncErrors(async(req,res,next)=>{
+    
+    const order = await Order.findById(req.params.id)
+    if (!order){
+        return next(new ErrorHandler("Product not found", 404))
+    }
+    await order.remove()
+    res.status(200).json({
+        success:true,
+    })
+})
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrderStatus, deleteOrder } = require("../controllers/orderController");
 const router = express.Router();
 const { isAuthenticated, authorizedRoles } = require('../middleware/auth');
 
@@ -8,5 +8,9 @@ router.route("/order/new").post(isAuthenticated, newOrder)
 router.route("/order/:id").get(isAuthenticated, getSingleOrder)
 
 router.route("/orders/me").get(isAuthenticated, myOrders)
+
+router.route("/admin/orders").get(isAuthenticated, authorizedRoles("admin"), getAllOrders)
+
+router.route("/admin/order/:id").put(isAuthenticated, authorizedRoles("admin"), updateOrderStatus).delete(isAuthenticated, authorizedRoles("admin"), deleteOrder)
 
 module.exports = router
